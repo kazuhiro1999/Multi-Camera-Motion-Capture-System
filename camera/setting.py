fix: default rotation & pose setting

camera/setting.py
@@ -22,14 +22,15 @@ import numpy as np
 
 class CameraSetting:
     DefaultPosition = np.zeros(3)
-    DefaultRotation = np.zeros(3)
+    DefaultRotation = np.eye(3,3)
     Forward = np.array([0,0,1])
-    def __init__(self, image_width=0, image_height=0, fov=90, position=DefaultPosition, rotation=DefaultRotation):
+    def __init__(self, image_width=0, image_height=0, fov=90, position=DefaultPosition, rotation=DefaultRotation, vector=Forward):
         self.image_width = image_width
         self.image_height = image_height
         self.fov = fov
-        self.position = position
-        self.rotation = rotation
+        self.position = position # 3d position (3)
+        self.rotation = rotation # rotation matrix (3x3)
+        self.vector = vector # forward vector (3)
         self.proj_matrix = self.get_projection_matrix()
 
     def set_intrinsic(self, image_width=None, image_height=None, FOV=None):
@@ -49,38 +50,55 @@ class CameraSetting:
         assert matrix.shape == (3,3)
         return matrix
 
-    def set_transform(self, position=None, rotation=None):
+    def set_transform(self, position=None, rotation=None, vector=None):
         if position is not None:
-            assert len(position) == 3
-            self.position = np.array(position).flatten()
+            position = np.array(position, dtype=np.float32)
+            if position.shape == (3,):
+                self.position = position
         if rotation is not None:
             rotation = np.array(rotation, dtype=np.float32)
             if rotation.shape == (3,3):
-                rotation = cv2.Rodrigues(rotation)[0]
-            assert len(rotation) == 3
-            self.rotation = np.array(rotation, dtype=np.float32).flatten()
+                self.rotation = rotation
+                self.vector = CameraSetting.calc_vector_from_rotation(rotation)
+            else:
+                print("rotation must be ndarray:(3,3)")
+        elif vector is not None:
+            vector = np.array(vector, dtype=np.float32)
+            if vector.shape == (3,):
+                self.vector = vector
+                self.rotation = CameraSetting.calc_rotation_from_vector(vector)
         self.proj_matrix = self.get_projection_matrix()
 
     def get_projection_matrix(self):
         K = self.get_camera_matrix()
         if K is None or self.position is None or self.rotation is None:
             return None
-        if np.linalg.norm(self.rotation) > 0:
-            vector = self.rotation / np.linalg.norm(self.rotation)
-            ax = (self.Forward + vector)
-            R = cv2.Rodrigues(ax/ np.linalg.norm(ax) * np.pi)[0]
-        else:
-            R = np.eye(3,3)
+        R = self.rotation
         t = self.position.reshape([3,1])
         Rc = R.T
         tc = -R.T @ t
         proj_matrix = K.dot(np.concatenate([Rc,tc], axis=1))
         return proj_matrix
-
+    
+    @staticmethod
+    def calc_rotation_from_vector(vector):
+        if np.linalg.norm(vector) == 0:
+            return np.eye(3,3)
+        vector = vector / np.linalg.norm(vector)
+        ax = (CameraSetting.Forward + vector)
+        R = cv2.Rodrigues(ax/ np.linalg.norm(ax) * np.pi)[0]
+        return R
+    
+    @staticmethod
+    def calc_vector_from_rotation(R):
+        return R @ CameraSetting.Forward.reshape(3,1)
 
 if __name__ == '__main__':
     camera_setting = CameraSetting()
     camera_setting.set_intrinsic(image_height=360, image_width=640, FOV=90)
-    camera_setting.set_transform(position=[-1.77,1.00,1.77], rotation=[1,0,-1])
+    camera_setting.set_transform(position=[-1.77,1.00,1.77], rotation=np.array([
+            [ 7.07106752e-01, -4.68652064e-17,  7.07106810e-01],
+            [ 4.68652064e-17, -1.00000000e+00, -1.13142610e-16], 
+            [ 7.07106810e-01,  1.13142610e-16, -7.07106752e-01]]))
 
-    print(camera_setting.get_projection_matrix())
+    print(camera_setting.vector)

pipeline.py
@@ -83,7 +83,7 @@ def load_camera_setting(camera, config):
     rotation_z = config['rotation']['z']  
     rotation = np.array([rotation_x, rotation_y, rotation_z])
     camera.camera_setting.set_intrinsic(FOV=FOV, image_height=image_height, image_width=image_width)
-    camera.camera_setting.set_transform(position=position, rotation=rotation)
+    camera.camera_setting.set_transform(position=position, vector=rotation)
     return camera.camera_setting
 
 def init_config(name="", camera_type='none', device_id=0, host='', segmentation='none', pose_estimation='none', debug=True):
@@ -106,7 +106,7 @@ def init_config(name="", camera_type='none', device_id=0, host='', segmentation=
             "rotation": {
                 "x": 0.0,
                 "y": 0.0,
-                "z": 0.0
+                "z": 1.0
             }
         },
         'segmentation':segmentation,
@@ -154,9 +154,9 @@ def update_config(config, camera=None, segmentation=None, pose_estimator=None):
                     'y' : float(camera.camera_setting.position[1]),
                     'z' : float(camera.camera_setting.position[2])}, 
                 'rotation' : {
-                    'x' : float(camera.camera_setting.rotation[0]),
-                    'y' : float(camera.camera_setting.rotation[1]),
-                    'z' : float(camera.camera_setting.rotation[2])}
+                    'x' : float(camera.camera_setting.vector[0]),
+                    'y' : float(camera.camera_setting.vector[1]),
+                    'z' : float(camera.camera_setting.vector[2])}
             }
         else:
             config['camera_setting'] = {}
@@ -358,9 +358,9 @@ class Editor:
             self.window['-PositionY-'].update(value=camera_setting.position[1])
             self.window['-PositionZ-'].update(value=camera_setting.position[2])
         if camera_setting.rotation is not None:
-            self.window['-RotationX-'].update(value=camera_setting.position[0])
-            self.window['-RotationY-'].update(value=camera_setting.rotation[1])
-            self.window['-RotationZ-'].update(value=camera_setting.rotation[2])
+            self.window['-RotationX-'].update(value=camera_setting.vector[0])
+            self.window['-RotationY-'].update(value=camera_setting.vector[1])
+            self.window['-RotationZ-'].update(value=camera_setting.vector[2])
             
     def apply_camera_setting(self, camera_setting:CameraSetting):
         if camera_setting is None or not self.isActive:
@@ -375,10 +375,10 @@ class Editor:
         rot_x = float(self.window['-RotationX-'].get())
         rot_y = float(self.window['-RotationY-'].get())
         rot_z = float(self.window['-RotationZ-'].get())
-        rotation = [rot_x, rot_y, rot_z]
+        vector = [rot_x, rot_y, rot_z]
 
         camera_setting.set_intrinsic(image_width=image_width, image_height=image_height, FOV=FOV)
-        camera_setting.set_transform(position=position, rotation=rotation)
+        camera_setting.set_transform(position=position, vector=vector)
         return True
     
     def get_segmentation_method(self):